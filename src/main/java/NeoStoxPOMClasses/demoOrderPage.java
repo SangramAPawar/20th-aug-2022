e5ab7408f62f477699b4a87a394fa61b6fc81093
sangram <sangramofficial01@gmail.com>
2023-02-03 18:43:16 +0530

    added demo order page

---
+++ src/main/java/NeoStoxPOMClasses/demoOrderPage.java
@@ -0,0 +1,26 @@
+package NeoStoxPOMClasses;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Reporter;
+
+public class demoOrderPage 
+{
+	@FindBy(xpath="(//a[text()='Sign In'])[1]")private WebElement SignInButton;
+	   
+	// initialize within the constructor with access level public using page factory
+   public demoOrderPage(WebDriver driver)
+   {
+	  PageFactory.initElements(driver, this); 
+   }
+   // utilize within a method with access level public
+   public void ClickOnSignin()
+   {
+	   SignInButton.click();
+	   Reporter.log("Clicking on signin button", true);
+
+	
+}
+}
